c35291cdb83b3360247ec1584a273b40926fbd02
KamandeRick <rickmwangi237@gmail.com>
2024-03-21 05:37:55 -0700

    make BaseModel class eith __init__ method

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"Modeule defining base class for all models"""
+from datetime import datetime
+import uuid
+import models
+
+class BaseModel:
+    """Base class on which all models are based"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+        else:
+            attribute_mapping = {
+                    'created_at': 'created_at',
+                    'id': 'id',
+                    'updated_at': 'updated_at'
+            }
+
+            for key, value in kwargs.items():
+                if key in attribute_mapping:
+                    attribute_name = attribute_mapping[key]
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.fromisoformat(value)
+                    setattr(self, attribute_name, value)
+
