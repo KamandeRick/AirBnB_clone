809901f6cccd6eca020dd0d9fac8e57e6f4c5132
KamandeRick <rickmwangi237@gmail.com>
2024-03-21 01:48:22 -0700

    add console docs test

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,7 +5,15 @@ from io import StringIO
 from console import HBNBcommand
 from unittest.mock import patch
 import unittest
+import console
 
 
 class TestConsole(unittest.TestCase):
     """ Testconsole class """
+    def test_ClassDocs(self):
+        """ Test to check if docs are present """
+        classDoc = __import__("console").HBNB.__doc__
+        self.assertGreater(len(classDoc), 0)
+
+    if __name__ == "__main__":
+        unittest.main()
