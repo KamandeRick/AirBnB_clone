73457cbe9f1a83f22a06110389a7d0d6cfc12d8a
KamandeRick <rickmwangi237@gmail.com>
2024-03-21 02:34:29 -0700

    add test to check for console module docs

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,9 +11,14 @@ import console
 class TestConsole(unittest.TestCase):
     """ Testconsole class """
     def test_ClassDocs(self):
-        """ Test to check if docs are present """
-        classDoc = __import__("console").HBNB.__doc__
+        """ Test to check if class docs are present """
+        classDoc = __import__("console").HBNBCommand.__doc__
         self.assertGreater(len(classDoc), 0)
+
+    def test_moduleDocs(self):
+        """ Test to check if module docs are present """
+        moduleDoc = __import__("console").__doc__
+        self.assertGreater(len(moduleDoc), 0)
 
     if __name__ == "__main__":
         unittest.main()
