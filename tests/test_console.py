09a09ad6760ae5d8f7c1e4aa6eda02659540ebcc
KamandeRick <rickmwangi237@gmail.com>
2024-03-21 01:28:27 -0700

    add readme authors and test folder

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Test for console """
+
+from io import StringIO
+from console import HBNBcommand
+from unittest.mock import patch
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """ Testconsole class """
