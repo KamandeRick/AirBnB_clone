2243ac09330566c44e5171da0288abadc990f018
KamandeRick <rickmwangi237@gmail.com>
2024-03-21 03:28:13 -0700

    add test to assert eof method works accordingly

--- tests/test_console.py
+++ tests/test_console.py
@@ -24,10 +24,17 @@ class TestConsole(unittest.TestCase):
         """ Test to check if help works as expected """
         with patch("sys.stdout", new=StringIO()) as output_stream:
             HBNBCommand().onecmd("help")
-            expected_outpt = """Documented commands (type help <topic>):
+            expected_output = """Documented commands (type help <topic>):
 ========================================
 Amenity    City  Place   State  all     destroy  quit  update
 BaseModel  EOF   Review  User   create  help     show"""
+            self.assertEqual(expected_output, output_stream.getvalue().strip())
+
+    def test_EOF(self):
+        """ Test if EOF method behaves as expected"""
+        with patch("sys.stdout", new=StringIO()) as output_stream:
+            HBNBCommand().onecmd("EOF")
+            expected_output = ""
             self.assertEqual(expected_output, output_stream.getvalue().strip())
 
     if __name__ == "__main__":
