a9ba275f7cfe7ece90163741c88f2902e4ea244c
KamandeRick <rickmwangi237@gmail.com>
2024-03-21 03:12:28 -0700

    make string io object more readable by descriptive variable name

--- tests/test_console.py
+++ tests/test_console.py
@@ -22,13 +22,13 @@ class TestConsole(unittest.TestCase):
 
     def test_help(self):
         """ Test to check if help works as expected """
-        with patch("sys.stdout", new=StringIO()) as f:
+        with patch("sys.stdout", new=StringIO()) as output_stream:
             HBNBCommand().onecmd("help")
             expected_outpt = """Documented commands (type help <topic>):
 ========================================
 Amenity    City  Place   State  all     destroy  quit  update
 BaseModel  EOF   Review  User   create  help     show"""
-            self.assertEqual(expected_output, f.getvalue().strip())
+            self.assertEqual(expected_output, output_stream.getvalue().strip())
 
     if __name__ == "__main__":
         unittest.main()
