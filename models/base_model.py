4fa4d0f4488044beacf676884305c8d061554c81
KamandeRick <rickmwangi237@gmail.com>
2024-03-22 02:05:34 -0700

    edit typos and comment out unecessary attributes

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
+            # models.storage.new(self)
         else:
             attribute_mapping = {
                     'created_at': 'created_at',
@@ -39,7 +39,7 @@ class BaseModel:
 
     def to_dict(self):
         """returns class represented as dictionary"""
-        dictionary = self.__dict__copy()
+        dictionary = self.__dict__.copy()
         dictionary["__class__"] = type(self).__name__
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
@@ -47,5 +47,5 @@ class BaseModel:
 
     def save(self):
         """updates updated_at with the current datetime"""
-        self.updated_at = dateime.now()
+        self.updated_at = datetime.now()
         # models.storage.save()
