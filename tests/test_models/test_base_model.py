ca306609db7556ab9c1a499fa2727a5736c2413a
KamandeRick <rickmwangi237@gmail.com>
2024-03-26 04:30:30 -0700

    add test for module and class docs

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,8 +3,29 @@
 import unittest
 from models.base_model import BaseModel
 from unittest.mock import patch
-from dtetime import datetime
+from datetime import datetime
 from uuid import UUID
 
-class TestBaseModel(unittest.TestCase):
+
+class Test_BaseModel(unittest.TestCase):
     """Basemodel class test"""
+    def setUp(self):
+        """method to setup env for each test case"""
+        self.base_model = BaseModel()
+
+    def tearDown(self):
+        """Clean up test environment as needed"""
+        pass
+
+    def test_moduleDoc(self):
+        """Test if module is documented"""
+        moduleDoc = __import__("models.base_model").base_model.__doc__
+        self.assertGreater(len(moduleDoc), 0)
+
+    def test_classDoc(self):
+        """test if classes are documented"""
+        classDoc = __import__("models.base_model").base_model.BaseModel.__doc__
+        self.assertGreater(len(classDoc), 0)
+
+    if __name__ == "__main__":
+        unittest.main()
