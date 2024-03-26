3f045107213b73dd19fc138fe5dcd70e6362bfd4
KamandeRick <rickmwangi237@gmail.com>
2024-03-26 10:28:36 -0700

    jik

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -29,7 +29,7 @@ class Test_BaseModel(unittest.TestCase):
 
     def test_id_is_string(self):
         """test id is type string"""
-        self.assertEqual(type(BaseModel().id), str)
+        self.assertEqual(str, type(BaseModel().id))
 
 
 if __name__ == "__main__":
