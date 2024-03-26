5b7e3285228f3a0bca1e17bf4d309ac5b99f81e8
KamandeRick <rickmwangi237@gmail.com>
2024-03-26 06:19:19 -0700

    docs

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"Modeule defining base class for all models"""
+"""Modeule defining base class for all models"""
 from datetime import datetime, date
 from uuid import uuid4
 import models
