ca8459526a665342c5eeac1ecc85857aeaa28409
Fred <rickmwangi237@gmail.com>
2024-03-31 05:15:52 -0700

    add hbnb class definition to console.py

--- console.py
+++ console.py
@@ -4,3 +4,28 @@ import cmd
 import sys
 import models
 from models.base_model import BaseModel
+from shlex import shlex
+from models.user import User
+from datetime import datetime
+from models.place import Place
+from models.review import Review
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Console program for airbnb clone"""
+    prompt = '(hbnb) '
+    class_list = {'BaseModel': BaseModel, 'State': State, 'City': City,
+               'Amenity': Amenity, 'Place': Place, 'Review': Review,
+               'User': User}
+
+    def emptyline(self):
+        """Method to ensure empty lines do not execute"""
+        pass
+
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
