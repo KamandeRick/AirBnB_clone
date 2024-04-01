3b1c1d85182ed3c58f509187b44db3e9b99bbd68
Fred <rickmwangi237@gmail.com>
2024-04-01 04:52:12 -0700

    add console file

--- console.py
+++ console.py
@@ -21,7 +21,7 @@ class HBNBCommand(cmd.Cmd):
                'Amenity': Amenity, 'Place': Place, 'Review': Review,
                'User': User}
 
-    def emptyline(self):
+    def empty_line(self):
         """Method to ensure empty lines do not execute"""
         pass
     
@@ -81,7 +81,66 @@ class HBNBCommand(cmd.Cmd):
                 del models.storage.all()[key]
                 models.storage.save()
 
+    def do_all(self, arg):
+        """Method to Print all instances based on the class name"""
+        if not arg:
+            print([str(value) for key, value in models.storage.all().items()])
+        else:
+            if not self.class_list.get(arg):
+                print("** class doesn't exist **")
+                return False
+            print([str(value) for key, value in models.storage.all().items()
+                   if type(value) is self.class_list.get(arg)])
 
+    def do_quit(self, arg):
+        """Method to quit and exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Method to exit program on EOF"""
+        return True
+
+    def default(self, line):
+        """Method to handle class commands"""
+        split_line = line.split('.', 1)
+        if len(ln) < 2:
+            print('*** Unknown syntax:', split_line[0])
+            return False
+        class_name, line = split_line[0], split_line[1]
+        if class_name not in list(self.class_list.keys()):
+            print('*** Unknown syntax: {}.{}'.format(class_name, line))
+            return False
+        split_line = line.split('(', 1)
+        if len(split_line) < 2:
+            print('*** Unknown syntax: {}.{}'.format(class_name, split_line[0]))
+            return False
+        method_name, args = split_line[0], split_line[1].rstrip(')')
+        if method_name not in ['all', 'count', 'show', 'destroy', 'update']:
+            print('*** Unknown syntax: {}.{}'.format(class_name, line))
+            return False
+        if method_name == 'all':
+            self.do_all(class_name)
+        elif method_name == 'count':
+            print(self.count_class(class_name))
+        elif method_name == 'show':
+            self.do_show(class_name + " " + args.strip('"'))
+        elif method_name == 'destroy':
+            self.do_destroy(class_name + " " + args.strip('"'))
+        elif method_name == 'update':
+            lb, rb = args.find('{'), args.find('}')
+            d = None
+            if args[lb:rb + 1] != '':
+                d = eval(args[lb:rb + 1])
+            split_line = args.split(',', 1)
+            obj_id, args = split_line[0].strip('"'), ln[1]
+            if d and type(d) is dict:
+                self.handle_dict(class_name, obj_id, d)
+            else:
+                from shlex import shlex
+                args = args.replace(',', ' ', 1)
+                split_line = list(shlex(args))
+                split_line[0] = split_line[0].strip('"')
+                self.do_update(" ".join([class_name, obj_id, split_line[0], split_line[1]]))
 
 
 if __name__ == "__main__":
