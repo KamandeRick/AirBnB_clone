fb815064bdbad5bfc76c21b1bc17f6dfab139aa9
Fred <rickmwangi237@gmail.com>
2024-04-02 03:06:31 -0700

    add update method to console

--- console.py
+++ console.py
@@ -105,6 +105,48 @@ class HBNBCommand(cmd.Cmd):
                     ]
             print(insts)
 
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        class_name = None
+        obj_id = None
+        attribute_name = None
+        attribute_value = None
+        updatetime = datetime.now()
+        args = arg.split(' ', 3)
+        if len(args) > 0:
+            class_name = args[0]
+        if len(args) > 1:
+            obj_id = args[1]
+        if len(args) > 2:
+            attribute_name = args[2]
+        if len(args) > 3:
+            attribute_value = list(shlex(args[3]))[0].strip('"')
+        if not class_name:
+            print('** class name missing **')
+        elif not obj_id:
+            print('** instance id missing **')
+        elif not attribute_name:
+            print('** attribute name missing **')
+        elif not attribute_value:
+            print('** value missing **')
+        elif not self.class_list.get(class_name):
+            print("** class doesn't exist **")
+        else:
+            key = class_name + "." + obj_id
+            obj = models.storage.all().get(key)
+            if not obj:
+                print('** no instance found **')
+            else:
+                if hasattr(obj, attribute_name):
+                    attribute_type = type(getattr(obj, attribute_name))
+                    attribute_value = attribute_type(attribute_value)
+                else:
+                    attribute_type = self.getType(attribute_value)
+                    attribute_value = attribute_type(attribute_value)
+                setattr(obj, attribute_name, attribute_value)
+                obj.updated_at = updatetime
+                models.storage.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
